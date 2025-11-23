dbf60f916d2de83653087dc4b89a33002feb5398
Nazneen Akter <nazneenzaman195@gmail.com>
2025-11-23 17:47:35 +0600

    fix

--- src/components/layout/Banner.jsx
+++ src/components/layout/Banner.jsx
@@ -41,9 +41,9 @@ const Banner = () => {
                 <div className="sm:ml-[20px] md:ml-[20px]">
                   <div className="flex items-center gap-x-4 mb-6">
                     <div className="bg-[#C32929] h-[2px] w-[40px] font-bold sm:text-[8px] text-[14px]"></div>
-                    <h3 className="font-jost text-[#C32929] font-medium leading-[24px] sm:text-[14px] text-[18px]">SUMMER 2025</h3>
+                    <h3 className="font-jost text-[#C32929] font-medium leading-[24px] sm:text-[14px] lg:text-[20px] text-[18px]">SUMMER 2025</h3>
                   </div>
-                  <h1 className="text-textC font-jost font-bold text-[75px] sm:text-[45px] leading-[80px]">HELLO NEW SEASON</h1>
+                  <h1 className="text-textC font-jost font-bold text-[75px] lg:text-[70px] sm:text-[45px] leading-[80px]">HELLO NEW SEASON</h1>
                   <p className="leading-[19px] text-[16px] text-textC font-medium mt-5 sm:text-[14px]">LIMITED TIME OFFER-UP TO 60% OFF & FREE SHIPPING</p>
                   <div className="mt-5 group inline-block">
                     <h3 className="text-textC mt-5 font-jost font-bold leading-[24px] text-[14px] sm:text-[14px] cursor-pointer group-hover:text-hoverC transition-all duration-300">DISCOVER MORE</h3>
@@ -71,9 +71,9 @@ const Banner = () => {
                 <div className="sm:ml-[20px] md:ml-[20px]">
                   <div className="flex items-center gap-x-4 mb-6">
                     <div className="bg-[#C32929] h-[2px] w-[40px] font-bold text-[14px] sm:text-[8px]"></div>
-                    <h3 className="font-jost text-[#C32929] font-medium leading-[24px] sm:text-[14px] text-[18px]">NEW TREND</h3>
+                    <h3 className="font-jost text-[#C32929] font-medium leading-[24px] sm:text-[14px] lg:text-[20px] text-[18px]">NEW TREND</h3>
                   </div>
-                  <h1 className="text-textC font-jost  text-[75px] leading-[80px] sm:text-[45px] font-bold">SUMMER SALE STYLISH </h1>
+                  <h1 className="text-textC font-jost  text-[75px] lg:text-[70px] leading-[80px] sm:text-[45px] font-bold">SUMMER SALE STYLISH </h1>
                   <p className="leading-[19px] text-[16px] text-textC font-medium mt-5 sm:text-[14px]">LIMITED TIME OFFER-UP TO 60% OFF & FREE SHIPPING</p>
                   <div className="mt-5 group inline-block">
                     <h3 className="text-textC mt-5 font-jost font-bold leading-[24px] text-[14px] cursor-pointer group-hover:text-hoverC transition-all duration-300 sm:text-[14px]">DISCOVER MORE</h3>
